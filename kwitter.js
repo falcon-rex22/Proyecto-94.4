bfcaa1da44a6251b51deea6aa2a9abc863aa1996
falcon-rex22 <120151153+falcon-rex22@users.noreply.github.com>
2023-04-28 18:48:33 -0600

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,5 @@
 function addUser(){
     var nombreUsuario = document.getElementById("user_name").value;
     localStorage.setItem("Usuario", nombreUsuario);
-    window.location="kwitter_room.html";
+    window.location="Kwitter_room.html";
 }
